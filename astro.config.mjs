f44fe15821c85b3333901037b749a165560b2057
Md. Naimur Rahman <mdnaimur020@gmail.com>
2026-09-11 21:49:19 +0600

    feat: add dotenv configuration and sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import "dotenv/config";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
+  site: process.env.SITE_URL || "https://example.com",
+  base: process.env.BASE_URL || "/",
+  integrations: [react(), sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
